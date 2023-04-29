da8d53ffec4560897e4ba236c10d3804b97e37f1
Joseph Rousoss <jroussos01@gmail.com>
2023-04-29 15:05:54 -0400

    this was not worth pushing

--- Assets/Scripts/Enemies/AiComponent.cs
+++ Assets/Scripts/Enemies/AiComponent.cs
@@ -7,29 +7,23 @@ public class AiComponent : MonoBehaviour
 {
     NavMeshAgent agent;
     GameObject target;
-    //EnemyShoot enemy_shoot;
     [SerializeField] bool isRanged = true;
     [SerializeField] float keepoutDistance = 7f; // this only applies for ranged enemies
-    private List<GameObject> possible_targets = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
-        //enemy_shoot = GetComponentInChildren<EnemyShoot>();
 
        
         target = GameObject.FindWithTag("Player"); 
         print(target);
-        //possible_targets.AddRange(machines);
-        //possible_targets.Add(target);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //chooseTarget(); // could limit this to once every few seconds...
         if (isRanged)
         {
             if((Vector3.Distance(this.transform.position, target.transform.position) > keepoutDistance /*|| !enemy_shoot.GetLineOfSight()*/))
@@ -47,34 +41,5 @@ public class AiComponent : MonoBehaviour
             agent.destination = target.transform.position;
         }
 
-    }
-
-    // if there are multiple targets, this will choose the closest one
-    private void chooseTarget()
-    {
-        float minDist;
-        GameObject newTarget = null;
-        if (possible_targets[0] != null)
-        {
-            minDist = Vector3.Distance(possible_targets[0].transform.position, transform.position);
-            newTarget = possible_targets[0];
-        }
-        else
-        {
-            minDist = float.PositiveInfinity;
-        }
-        foreach (GameObject g in possible_targets)
-        {
-            if (g != null)
-            {
-                float distance = Vector3.Distance(g.transform.position, transform.position);
-                if (distance < minDist)
-                {
-                    minDist = distance;
-                    newTarget = g;
-                }
-            }
-        }
-        target = newTarget;
     }
 }
