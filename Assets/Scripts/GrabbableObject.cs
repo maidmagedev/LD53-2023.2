b5c57d56163a0a47a8d521db2b50f68abca16297
andycofsky <ajlitcofsky@gmail.com>
2023-04-29 16:03:44 -0400

    Added GrabbableObject script

---
+++ Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour
+{
+    public GameObject parent;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
