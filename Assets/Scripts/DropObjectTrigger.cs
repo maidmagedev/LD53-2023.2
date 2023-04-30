9173ed2b145dc344f4e36663a37270d0dd4db1fa
maidmage <46210759+maidmagedev@users.noreply.github.com>
2023-04-30 01:48:43 -0400

    SFX, + Sfx added to Forced Drop Trigger

--- Assets/Scripts/DropObjectTrigger.cs
+++ Assets/Scripts/DropObjectTrigger.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class DropObjectTrigger : TriggerBase
 {
     [SerializeField] GrabberObject gObj;
+
+    [Header("Audio")]
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip releaseNoise;
+    [SerializeField] float volume;
+    
     public override void DoAction() {
         if (gObj == null) {
             gObj = FindObjectOfType<GrabberObject>();
         }
         if (gObj.isReleaseReady) {
+            audioSource.clip = releaseNoise;
+            audioSource.volume = volume;
+            audioSource.Play();
             gObj.Release();
         }
     }
